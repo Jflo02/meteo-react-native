2dabfe754f87ded02d711edbd05b1a514aec6c34
Piett-rm <rmpetit24@gmail.com>
2022-01-31 22:12:22 +0100

    ajout appel api

---
+++ app/controller/controller.js
@@ -0,0 +1,16 @@
+import { Api } from '../models/Api';
+
+
+class controller {
+
+
+    FeatherForecast(research){
+        
+        let obj = new Api();
+        valueReseach = obj.search(research);
+
+    }
+
+
+
+}
