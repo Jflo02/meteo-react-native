bf30bbea29902ee87a096eadd659affa8bf50c56
flori <florimond2002@gmail.com>
2022-01-31 20:30:12 +0100

    adding searchBar in main page

--- app/views/App.js
+++ app/views/App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "./SearchBar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ margin: 20 }}>
+        <Text style={styles.text}>
+          Bienvenue sur votre application météo favorite Entrez une ville
+        </Text>
+        <SearchBar />
+      </View>
     </View>
   );
 }
@@ -14,8 +19,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: `rgb(63,94,251)`,
   },
+  text: {
+    margin: 20,
+    marginBottom: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchBar: {},
 });
